df81ac3d6719e75c7924fb61b7fab9aa97d16e67
衣斐司郎 <shiro@sunyou.co.jp>
2021-01-11 16:34:33 +0900

    comment修正

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 症状一覧
 Route::get('syojo', 'SyojoController@index');
 
+// 不良箇所一覧
 Route::get('part', 'PartController@index');
 
+// 不良箇所追加処理
 Route::get('part/add', 'PartController@add');
 Route::post('part/add', 'PartController@create');
 
+// 原因一覧表示
 Route::get('cause', 'CauseController@index');
 
+// 所見一覧表示
 Route::get('comment', 'CommentController@index');
 
+// 全レコードを横に一覧表示
 Route::get('all', 'AllController@index');
+
+// テーブルの関連性
+Route::get('relation', 'RelationController@index');
 
 
 
